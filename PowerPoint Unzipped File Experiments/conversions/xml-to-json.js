6a9b7341849ac1ed61085592baeed28133a4fe8a
Greg Dolley <gregdolley@gmail.com>
2017-12-06 03:44:38 -0800

    More XML to JSON stub conversions.

--- PowerPoint Unzipped File Experiments/conversions/xml-to-json.js
+++ PowerPoint Unzipped File Experiments/conversions/xml-to-json.js
@@ -1,9 +1,11 @@
 let convert = require('xml-js');
 let fs = require('fs');
 
-let xml_to_convert = fs.readFileSync('core.xml');
-let json_object = convert.xml2json(xml_to_convert, { ignoreComment: true, alwaysChildren: true, compact: false });
+for (let i = 1; i <= 11; i++) {
+	let xml_to_convert = fs.readFileSync(`slideLayout${i}.xml`);
+	let json_object = convert.xml2json(xml_to_convert, { ignoreComment: true, alwaysChildren: true, compact: false });
 
-fs.writeFileSync(`${__dirname  }/core.xml.js`, json_object.toString());
+	fs.writeFileSync(`${__dirname}/slideLayout${i}.xml.js`, json_object.toString());
+}
 
-console.log("Done.");
+console.log('Done.');
