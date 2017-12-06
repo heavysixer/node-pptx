e02830b8a7ceb0adb9be0505fb5527360eb782e8
Greg Dolley <gregdolley@gmail.com>
2017-12-06 05:36:18 -0800

    More stub code.

--- PowerPoint Unzipped File Experiments/conversions/xml-to-json.js
+++ PowerPoint Unzipped File Experiments/conversions/xml-to-json.js
@@ -1,11 +1,17 @@
 let convert = require('xml-js');
 let fs = require('fs');
 
-for (let i = 1; i <= 11; i++) {
-	let xml_to_convert = fs.readFileSync(`slideLayout${i}.xml`);
-	let json_object = convert.xml2json(xml_to_convert, { ignoreComment: true, alwaysChildren: true, compact: false });
+// for (let i = 1; i <= 11; i++) {
+// 	let xml_to_convert = fs.readFileSync(`slideLayout${i}.xml`);
+// 	let json_object = convert.xml2json(xml_to_convert, { ignoreComment: true, alwaysChildren: true, compact: false });
+//
+// 	fs.writeFileSync(`${__dirname}/slideLayout${i}.xml.js`, json_object.toString());
+// }
 
-	fs.writeFileSync(`${__dirname}/slideLayout${i}.xml.js`, json_object.toString());
-}
+let xml_to_convert = fs.readFileSync(`slideMaster1.stub.xml`);
+let json_object = convert.xml2json(xml_to_convert, { ignoreComment: true, alwaysChildren: true, compact: false });
+
+fs.writeFileSync(`${__dirname}/slideMaster1.xml.js`, json_object.toString());
+
 
 console.log('Done.');
