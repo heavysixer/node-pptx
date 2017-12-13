1282e7b7735a9880c8f972e0abfb849fca179ced
Greg Dolley <gregdolley@gmail.com>
2017-12-12 21:12:41 -0800

    Big refactor. Ability to add slides to _existing_ pptx working. Images not done yet, but I've changed too much code not to have a backup somewhere...

--- __tests__/presentation.test.js
+++ __tests__/presentation.test.js
@@ -4,70 +4,89 @@ const path = require('path');
 const tmpDir = `${__dirname}/tmp`;
 
 describe('Presentation Module', () => {
-	beforeAll(() => {
-		prepareTmpDir(tmpDir);
-	});
+    beforeAll(() => {
+        prepareTmpDir(tmpDir);
+    });
 
-	afterAll(() => {
-		// emptyDir(tmpDir);
-	});
+    afterAll(() => {
+        // emptyDir(tmpDir);
+    });
 
-	test('should be able to load an existing pptx file', () => {
-		try {
-			let fulltemplateFilePath = `${__dirname}/fixtures/basic.pptx`;
-			let presentation = new PPTX.Presentation({ templateFilePath: fulltemplateFilePath });
+    test('should be able to load an existing pptx file', () => {
+        try {
+            let fulltemplateFilePath = `${__dirname}/fixtures/basic.pptx`;
+            let presentation = new PPTX.Presentation({ templateFilePath: fulltemplateFilePath });
 
-			presentation.loadExistingPPTX(function(err) {
-				if (err) fail(err);
+            presentation.loadExistingPPTX(function(err) {
+                if (err) fail(err);
 
-				presentation.save(`${tmpDir}/rewrite-of-existing.pptx`);
-				expect(fs.existsSync(`${tmpDir}/rewrite-of-existing.pptx`)).toBe(true);
-			});
-		} catch (err) {
-			console.log(err);
-			throw err;
-		}
-	});
+                presentation.save(`${tmpDir}/rewrite-of-existing.pptx`);
+                expect(fs.existsSync(`${tmpDir}/rewrite-of-existing.pptx`)).toBe(true);
+            });
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    });
 
-	test('should be able to create a pptx file from scratch', () => {
-		try {
-			let presentation = new PPTX.Presentation();
+    test('should be able to load an existing pptx file and add another slide', () => {
+        try {
+            let fulltemplateFilePath = `${__dirname}/fixtures/basic.pptx`;
+            let presentation = new PPTX.Presentation({ templateFilePath: fulltemplateFilePath });
 
-			presentation.buildPowerPoint(); // TODO: this function is needed for init, but is also used for building after adding objects to a pptx. Separate into two different functions...
-			let newSlide = presentation.addSlide('slide2', 'slideLayout2');
+            presentation.loadExistingPPTX(function(err) {
+                if (err) fail(err);
 
-			expect(newSlide.rId).toBeDefined();
-			expect(newSlide.rId).not.toBeNull();
+                presentation.addSlide();
+                presentation.save(`${tmpDir}/add-slide-to-existing.pptx`);
+                expect(fs.existsSync(`${tmpDir}/add-slide-to-existing.pptx`)).toBe(true);
+            });
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    });
 
-			presentation.addSlide('slide3!', 'slideLayout3');
-			presentation.addSlide('slide4', 'slideLayout4');
-			presentation.addSlide('Another', 'slideLayout5');
-			presentation.addSlide('Woohoo!', 'slideLayout6');
-			presentation.addSlide('HelloWorld!', 'slideLayout7');
-			presentation.addSlide('Last', 'slideLayout8');
+    test('should be able to create a pptx file from scratch', () => {
+        try {
+            let presentation = new PPTX.Presentation();
 
-			presentation.save(`${tmpDir}/multiple_slides.pptx`);
+            presentation.buildPowerPoint(); // TODO: this function is needed for init, but is also used for building after adding objects to a pptx. Separate into two different functions...
 
-			expect(fs.existsSync(`${tmpDir}/multiple_slides.pptx`)).toBe(true);
-		} catch (err) {
-			console.log(err);
-			throw err;
-		}
-	});
+            let newSlide = presentation.addSlide();
+
+            expect(newSlide.rId).toBeDefined();
+            expect(newSlide.rId).not.toBeNull();
+
+            presentation.addSlide('slideLayout3');
+            presentation.addSlide('slideLayout4');
+            presentation.addSlide('slideLayout5');
+            presentation.addSlide('slideLayout6');
+            presentation.addSlide('slideLayout7');
+            presentation.addSlide('slideLayout8');
+
+            presentation.save(`${tmpDir}/multiple_slides.pptx`);
+
+            expect(fs.existsSync(`${tmpDir}/multiple_slides.pptx`)).toBe(true);
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    });
 });
 
 function prepareTmpDir(dir) {
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir);
-	} else {
-		emptyDir(dir);
-	}
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    } else {
+        emptyDir(dir);
+    }
 }
 
 function emptyDir(dir) {
-	for (const file of fs.readdirSync(dir)) {
-		fs.unlink(path.join(dir, file), err => {
-			if (err) throw err;
-		});
-	}
+    for (const file of fs.readdirSync(dir)) {
+        fs.unlink(path.join(dir, file), err => {
+            if (err) throw err;
+        });
+    }
 }
