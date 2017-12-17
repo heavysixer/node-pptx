32d62a3350cfd0f7c954280142bf585f766fd92c
Greg Dolley <gregdolley@gmail.com>
2017-12-17 15:40:56 -0800

    Added shape types and updated tests.

--- __tests__/shapes.test.js
+++ __tests__/shapes.test.js
@@ -23,15 +23,16 @@ describe('Presentation Module', () => {
             expect(slide1.content).toBeDefined();
             expect(slide1.content).not.toBeNull();
 
-            slide1.addShape('triangle', { x: 50, y: 50, cx: 50, cy: 50 });
-            slide1.addShape('triangle', { x: 150, y: 50, cx: 50, cy: 50, color: '00FF00' });
-            slide1.addShape('ellipse', { x: 100, y: 200, cx: 200, cy: 100, text: 'hello world!' });
-            slide1.addShape('trapezoid', { x: 320, y: 200, cx: 150, cy: 150, color: 'FF0000' });
-            slide1.addShape('leftArrow', { x: 500, y: 340, cx: 100, cy: 50, color: 'FF00FF' });
+            slide1.addShape(PPTX.ShapeTypes.TRIANGLE, { x: 50, y: 50, cx: 50, cy: 50 });
+            slide1.addShape(PPTX.ShapeTypes.TRIANGLE, { x: 150, y: 50, cx: 50, cy: 50, color: '00FF00' });
+            slide1.addShape(PPTX.ShapeTypes.OVAL, { x: 100, y: 200, cx: 200, cy: 100, text: 'hello world!' });
+            slide1.addShape(PPTX.ShapeTypes.ELLIPSE, { x: 450, y: 200, cx: 200, cy: 100, text: 'hello world!', color: '00FF00' }); // testing alias (ELLIPSE == OVAL)
+            slide1.addShape(PPTX.ShapeTypes.TRAPEZOID, { x: 320, y: 200, cx: 150, cy: 150, color: 'FF0000' });
+            slide1.addShape(PPTX.ShapeTypes.LEFT_ARROW, { x: 500, y: 340, cx: 100, cy: 50, color: 'FF00FF' });
 
             slide1
-                .addShape('rightArrow', { x: 500, y: 440, cx: 100, cy: 50, color: '0000FF' })
-                .addShape('upArrow', { x: 500, y: 140, cx: 100, cy: 50, color: '0000FF', url: 'www.google.com' });
+                .addShape(PPTX.ShapeTypes.RIGHT_ARROW, { x: 500, y: 440, cx: 100, cy: 50, color: '0000FF' })
+                .addShape(PPTX.ShapeTypes.UP_ARROW, { x: 500, y: 140, cx: 100, cy: 50, color: '0000FF', url: 'www.google.com' });
 
             presentation.save(`${tmpDir}/shapes-test.pptx`);
             expect(fs.existsSync(`${tmpDir}/shapes-test.pptx`)).toBe(true);
--- __tests__/smoke.test.js
+++ __tests__/smoke.test.js
@@ -1,6 +1,6 @@
 const PPTX = require('../index.js');
 describe('test essentials', () => {
     test('true', () => {
-        expect(Object.keys(PPTX)).toEqual(['Presentation', 'Slide']);
+        expect(Object.keys(PPTX)).toEqual(['Presentation', 'Slide', 'ShapeTypes']);
     });
 });
