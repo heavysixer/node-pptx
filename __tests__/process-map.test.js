5d0f693e6bfbdffcd4c1d269686ad584cc331212
Greg Dolley <gregdolley@gmail.com>
2017-12-22 00:14:37 -0800

    Recreating process map from scratch.

--- __tests__/process-map.test.js
+++ __tests__/process-map.test.js
@@ -2,6 +2,8 @@ const PPTX = require('../index.js');
 const fs = require('fs');
 const tmpDir = `${__dirname}/tmp`;
 
+let { PptxUnitHelper } = require('../lib/helpers/unit-helper');
+
 describe('Presentation Module', () => {
     test('should be able to load an existing pptx file', () => {
         try {
@@ -16,63 +18,60 @@ describe('Presentation Module', () => {
                 expect(slide1.content).toBeDefined();
                 expect(slide1.content).not.toBeNull();
 
-                //                slide1.addShape(PPTX.ShapeTypes.TRIANGLE, { x: 50, y: 50, cx: 50, cy: 50 });
-                //                slide1.addText('Hello world!');
+                let slide2 = presentation.addSlide();
 
-                // slide1.addShape(PPTX.ShapeTypes.TRIANGLE, { x: 150, y: 50, cx: 50, cy: 50, color: '00FF00' });
-                // slide1.addShape(PPTX.ShapeTypes.OVAL, { x: 100, y: 200, cx: 200, cy: 100, text: 'hello world!' });
-                // slide1.addShape(PPTX.ShapeTypes.ELLIPSE, { x: 450, y: 200, cx: 200, cy: 100, text: 'hello world!', color: '00FF00' }); // testing alias (ELLIPSE == OVAL)
-                // slide1.addShape(PPTX.ShapeTypes.TRAPEZOID, { x: 320, y: 200, cx: 150, cy: 150, color: 'FF0000' });
-                // slide1.addShape(PPTX.ShapeTypes.LEFT_ARROW, { x: 500, y: 340, cx: 100, cy: 50, color: 'FF00FF' });
-                //
-                // slide1
-                //     .addShape(PPTX.ShapeTypes.RIGHT_ARROW, { x: 500, y: 440, cx: 100, cy: 50, color: '0000FF' })
-                //     .addShape(PPTX.ShapeTypes.UP_ARROW, { x: 500, y: 140, cx: 100, cy: 50, color: '0000FF', url: 'www.google.com' });
-                //
-                // //----------------------------------------------------------------------------------------------------------------------------
-                // // copied from anotehr test, let's see if we can overlay everything and not get a corrupt file...
-                // // (and, yes, the resulting first slide will look stupid...)
-                // slide1.addText('Hello world!');
-                // slide1.addShape(PPTX.ShapeTypes.TRIANGLE, { x: 400, cx: 100, cy: 100, url: 'http://www.google.com' });
-                // slide1.addText('This is a hyperlink! Will this go to google?', { x: 0, y: 25, cx: 400, url: 'http://www.google.com' });
-                // slide1.addText('Will this go to slide 3?', { x: 0, y: 50, url: '#3' });
-                //
-                // slide1
-                //     .addText('Another piece of text, non-default position, wide block.')
-                //     .x(100)
-                //     .y(100)
-                //     .cx(500)
-                //     .cy(50);
-                //
-                // slide1
-                //     .addText('Text in skinny block, this should wrap.')
-                //     .x(20)
-                //     .y(150)
-                //     .cx(200)
-                //     .cy(150);
-                //
-                // slide1
-                //     .addText('Non-Latin (Cyrillic) character test: Привет мир!')
-                //     .x(300)
-                //     .y(150)
-                //     .cx(400)
-                //     .cy(150)
-                //     .addText("Let's go crazy: оалмгцнйукрлмьтсмщфзйудлтлваывувыаитыбюяй", { x: 300, y: 175, cx: 400 });
-                //
-                // slide1
-                //     .addImage(`${__dirname}/images/pizza.jpg`, { url: '#3' })
-                //     .x(100)
-                //     .y(200)
-                //     .cx(166)
-                //     .cy(100);
-                //
-                // slide1
-                //     .addImage(`${__dirname}/images/image1.png`)
-                //     .x(400)
-                //     .y(250)
-                //     .cx(250)
-                //     .cy(150);
-                //----------------------------------------------------------------------------------------------------------------------------
+                slide2.addShape(PPTX.ShapeTypes.TRIANGLE, { x: 50, y: 50, cx: 50, cy: 50 });
+                slide2.addText('Hello world!');
+
+                slide2.addShape(PPTX.ShapeTypes.TRIANGLE, { x: 150, y: 50, cx: 50, cy: 50, color: '00FF00' });
+                slide2.addShape(PPTX.ShapeTypes.OVAL, { x: 100, y: 200, cx: 200, cy: 100, text: 'hello world!' });
+                slide2.addShape(PPTX.ShapeTypes.ELLIPSE, { x: 450, y: 200, cx: 200, cy: 100, text: 'hello world!', color: '00FF00' }); // testing alias (ELLIPSE == OVAL)
+                slide2.addShape(PPTX.ShapeTypes.TRAPEZOID, { x: 320, y: 200, cx: 150, cy: 150, color: 'FF0000' });
+                slide2.addShape(PPTX.ShapeTypes.LEFT_ARROW, { x: 500, y: 340, cx: 100, cy: 50, color: 'FF00FF' });
+
+                slide2.addShape(PPTX.ShapeTypes.RIGHT_ARROW, { x: 500, y: 440, cx: 100, cy: 50, color: '0000FF' });
+                slide2.addShape(PPTX.ShapeTypes.UP_ARROW, { x: 500, y: 140, cx: 100, cy: 50, color: '0000FF', url: 'www.google.com' });
+
+                slide2.addText('Hello world!');
+                slide2.addShape(PPTX.ShapeTypes.TRIANGLE, { x: 400, cx: 100, cy: 100, url: 'http://www.google.com' });
+                slide2.addText('This is a hyperlink! Will this go to google?', { x: 0, y: 25, cx: 400, url: 'http://www.google.com' });
+                slide2.addText('Will this go to slide 3?', { x: 0, y: 50, url: '#3' });
+
+                slide2
+                    .addText('Another piece of text, non-default position, wide block.')
+                    .x(100)
+                    .y(100)
+                    .cx(500)
+                    .cy(50);
+
+                slide2
+                    .addText('Text in skinny block, this should wrap.')
+                    .x(20)
+                    .y(150)
+                    .cx(200)
+                    .cy(150);
+
+                slide2
+                    .addText('Non-Latin (Cyrillic) character test: Привет мир!')
+                    .x(300)
+                    .y(150)
+                    .cx(400)
+                    .cy(150)
+                    .addText("Let's go crazy: оалмгцнйукрлмьтсмщфзйудлтлваывувыаитыбюяй", { x: 300, y: 175, cx: 400 });
+
+                slide2
+                    .addImage(`${__dirname}/images/pizza.jpg`, { url: '#3' })
+                    .x(100)
+                    .y(200)
+                    .cx(166)
+                    .cy(100);
+
+                slide2
+                    .addImage(`${__dirname}/images/image1.png`)
+                    .x(400)
+                    .y(250)
+                    .cx(250)
+                    .cy(150);
 
                 // trying to access the process template data via our object tree and some direct XML injection:
                 // let shape = slide1.getShapeObject(0);
@@ -99,14 +98,13 @@ describe('Presentation Module', () => {
                     if (shapeContent['p:txBody'] && shapeContent['p:txBody'][0]['a:p']) {
                         if (shapeContent['p:txBody'][0]['a:p'][0]['a:r']) {
                             shapeContent['p:txBody'][0]['a:p'][0]['a:r'][0]['a:t'] = "Shape's new text!";
-                            shapeContent['p:txBody'][0]['a:p'][0]['a:endParaRPr'] = [{ $: { lang: 'en-US' } }]; // this MUST go last!
+                            //shapeContent['p:txBody'][0]['a:p'][0]['a:endParaRPr'] = [{ $: { lang: 'en-US' } }]; // this MUST go last!
                         } else {
                             shapeContent['p:txBody'][0]['a:p'][0]['a:r'] = [
                                 { 'a:rPr': [{ $: { lang: 'en-US', smtClean: '0' } }], 'a:t': 'Mod our own shape' },
                             ];
 
-                            // major problem: this node is NOT being written out last in the node order! Causes a corrupt file.
-                            shapeContent['p:txBody'][0]['a:p'][0]['a:endParaRPr'] = [{ $: { lang: 'en-US' } }]; // this MUST go last!
+                            //shapeContent['p:txBody'][0]['a:p'][0]['a:endParaRPr'] = [{ $: { lang: 'en-US' } }]; // this MUST go last!
                         }
                     }
                 } else {
@@ -116,6 +114,42 @@ describe('Presentation Module', () => {
                 presentation.save(`${tmpDir}/process-map-rewrite.pptx`);
                 expect(fs.existsSync(`${tmpDir}/process-map-rewrite.pptx`)).toBe(true);
             });
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    });
+
+    test('should be able to create the process map from scratch', () => {
+        try {
+            let presentation = new PPTX.Presentation();
+            presentation.buildPowerPoint();
+
+            let slide1 = presentation.getSlide('slide1');
+
+            expect(slide1.content).toBeDefined();
+            expect(slide1.content).not.toBeNull();
+
+            // TODO's to support everything:
+            //
+            // -Need to add different slide dimensions (like the 16x9 layout)
+            // -Need to add line color and line style support to shapes (so we can make those vertical dotted line separators)
+            // -Need to add font support to text
+            // -Need to add ability to center text
+            // -Optional: theme color support would be nice
+            // -Add line color to shapes (this is the same as the border color)
+
+            slide1.addText('Process map template', { x: 100, y: 50, cx: 500, cy: 20 });
+            slide1.addShape(PPTX.ShapeTypes.PENTAGON, {
+                x: PptxUnitHelper.toPixels(PptxUnitHelper.fromInches(3.75)),
+                y: PptxUnitHelper.toPixels(PptxUnitHelper.fromInches(1.23)),
+                cx: PptxUnitHelper.toPixels(PptxUnitHelper.fromInches(1.24)),
+                cy: PptxUnitHelper.toPixels(PptxUnitHelper.fromInches(1.16)),
+                color: 'C5F484',
+            });
+
+            presentation.save(`${tmpDir}/new-process-map.pptx`);
+            expect(fs.existsSync(`${tmpDir}/new-process-map.pptx`)).toBe(true);
         } catch (err) {
             console.log(err);
             throw err;
