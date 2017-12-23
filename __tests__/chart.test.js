129a595dad3ee346e4bbbc6bbb44482c0e07fe47
Greg Dolley <gregdolley@gmail.com>
2017-12-23 09:45:26 -0800

    Got two charts working on two slides. :D

--- __tests__/chart.test.js
+++ __tests__/chart.test.js
@@ -45,6 +45,39 @@ describe('Presentation Module', () => {
 
             await slide1.addChart(barChartData, { x: 100, y: 100, cx: 400, cy: 300 });
 
+            let slide2 = presentation.addSlide();
+
+            barChartData.title = 'Sample chart #2';
+            barChartData.data = [
+                {
+                    name: 'Series 1',
+                    labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4'],
+                    values: [1.3, 4.5, 3.5, 4.5],
+                },
+                {
+                    name: 'Series 2',
+                    labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4'],
+                    values: [6.4, 2.4, 3.8, 5.8],
+                },
+                {
+                    name: 'Series 3',
+                    labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4'],
+                    values: [1.2, 3.7, 2.5, 1.0],
+                },
+                {
+                    name: 'Series 4',
+                    labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4'],
+                    values: [2.4, 0.8, 0.2, 6.0],
+                },
+                {
+                    name: 'Series 5',
+                    labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4'],
+                    values: [3.0, 4.0, 5.0, 6.0],
+                },
+            ];
+
+            await slide2.addChart(barChartData, { x: 0, y: 0, cx: 600, cy: 400 });
+
             presentation.save(`${tmpDir}/chart.pptx`);
             expect(fs.existsSync(`${tmpDir}/chart.pptx`)).toBe(true);
         } catch (err) {
