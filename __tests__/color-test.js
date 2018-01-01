d876703ce93f52490c8bb49e4c0e2380164f6df3
Greg Dolley <gregdolley@gmail.com>
2018-01-01 03:20:45 -0800

    Support for global/default text color.

--- __tests__/color-test.js
+++ __tests__/color-test.js
@@ -4,7 +4,7 @@ const tmpDir = `${__dirname}/tmp`;
 
 describe('Presentation Module', () => {
     describe('with an existing pptx file', () => {
-        test('should be able to add another slide and change the background color of each slide', () => {
+        test('should be able to add another slide, change the background color of each slide, and add a default text foreground color', () => {
             try {
                 if (fs.existsSync(`${tmpDir}/presentation-existing-non-default-colors.pptx`)) {
                     fs.unlinkSync(`${tmpDir}/presentation-existing-non-default-colors.pptx`);
@@ -17,9 +17,16 @@ describe('Presentation Module', () => {
                     if (err) fail(err);
 
                     let slide = presentation.getSlide('slide1');
+                    slide.setTextColor('00AAFF');
                     slide.setBackgroundColor('C5E0B4');
+                    slide.addText('Hello world!', { x: 450, y: 100, cx: 400, cy: 25, fontSize: 24 });
+
                     slide = presentation.addSlide();
                     slide.setBackgroundColor('FFD777');
+
+                    slide.setTextColor('FF0000');
+                    slide.addText('Hello world!');
+
                     presentation.save(`${tmpDir}/presentation-existing-non-default-colors.pptx`);
                     expect(fs.existsSync(`${tmpDir}/presentation-existing-non-default-colors.pptx`)).toBe(true);
                 });
@@ -31,7 +38,7 @@ describe('Presentation Module', () => {
     });
 
     describe('when creating a presentation without an existing file', () => {
-        test('should be able to create a slide with a non-default background color', () => {
+        test('should be able to create a slide with a non-default background color and set a default text foreground color', () => {
             try {
                 if (fs.existsSync(`${tmpDir}/presentation-new-non-default-colors.pptx`)) {
                     fs.unlinkSync(`${tmpDir}/presentation-new-non-default-colors.pptx`);
@@ -43,7 +50,10 @@ describe('Presentation Module', () => {
                 expect(slide.content).toBeDefined();
                 expect(slide.content).not.toBeNull();
 
+                slide.setTextColor('00AAFF');
                 slide.setBackgroundColor('FFD777');
+
+                slide.addText('Hello World!');
 
                 slide = presentation.addSlide();
 
