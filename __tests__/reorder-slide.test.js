294995853737de6ceb76cdbc5dcc1f08e2d40baf
gregdolley <gregdolley@gmail.com>
2018-09-07 22:13:10 -0700

    Added support for slide layout reordering.

--- __tests__/reorder-slide.test.js
+++ __tests__/reorder-slide.test.js
@@ -17,10 +17,22 @@ describe('Presentation Module', () => {
 
             await pptx.load(`${__dirname}/fixtures/three-slides.pptx`);
             await pptx.compose(async pres => {
+                let slideTest2 = pres.getSlide(2);
+                let slideTest3 = pres.getSlide(3);
+
+                slideTest2.layoutName = 'slideLayout2';
+                slideTest3.layoutName = 'slideLayout3';
+
                 let slide = pres.getSlide(3);
                 slide.moveTo(2);
 
-                slide.addText({ value: 'If this slide says "original slide #3" but is in slide position 2, then it worked.', x: 25, y: 200 });
+                slide.addText(text => {
+                    text
+                        .value('If this slide says "original slide #3" but is in slide position 2, then it worked.')
+                        .x(25)
+                        .y(200)
+                        .cx(600);
+                });
             });
 
             await pptx.save(`${tmpDir}/${testFileName}`);
