1489236aee30b242452c312aa600a8d118277f14
gregdolley <gregdolley@gmail.com>
2018-10-05 23:48:54 -0700

    Backup check-in for slide reordering feature.

--- __tests__/reorder-slide.test.js
+++ __tests__/reorder-slide.test.js
@@ -5,7 +5,7 @@ const tmpDir = `${__dirname}/tmp`;
 describe('Presentation Module', () => {
     test("should be able to load an existing PPTX (containing three slides) and swap slide #'s 2 and 3", async () => {
         try {
-            let testFileName = 'presentation-swap-existing-slides.pptx';
+            let testFileName = 'presentation-swap-existing-slides-small.pptx';
 
             expect.assertions(1);
 
@@ -17,12 +17,6 @@ describe('Presentation Module', () => {
 
             await pptx.load(`${__dirname}/fixtures/three-slides.pptx`);
             await pptx.compose(async pres => {
-                let slideTest2 = pres.getSlide(2);
-                let slideTest3 = pres.getSlide(3);
-
-                slideTest2.layoutName = 'slideLayout2';
-                slideTest3.layoutName = 'slideLayout3';
-
                 let slide = pres.getSlide(3);
                 slide.moveTo(2);
 
@@ -33,6 +27,58 @@ describe('Presentation Module', () => {
                         .y(200)
                         .cx(600);
                 });
+            });
+
+            await pptx.save(`${tmpDir}/${testFileName}`);
+            expect(fs.existsSync(`${tmpDir}/${testFileName}`)).toBe(true);
+        } catch (err) {
+            console.warn(err);
+            throw err;
+        }
+    });
+
+    test('should be able to load an existing PPTX (containing six slides) and move slide #5 to slide #2', async () => {
+        try {
+            let testFileName = 'presentation-swap-existing-slides-large-5-to-2.pptx';
+
+            expect.assertions(1);
+
+            if (fs.existsSync(`${tmpDir}/${testFileName}`)) {
+                fs.unlinkSync(`${tmpDir}/${testFileName}`);
+            }
+
+            let pptx = new PPTX.Composer();
+
+            await pptx.load(`${__dirname}/fixtures/six-slides.pptx`);
+            await pptx.compose(async pres => {
+                let slide = pres.getSlide(5);
+                slide.moveTo(2);
+            });
+
+            await pptx.save(`${tmpDir}/${testFileName}`);
+            expect(fs.existsSync(`${tmpDir}/${testFileName}`)).toBe(true);
+        } catch (err) {
+            console.warn(err);
+            throw err;
+        }
+    });
+
+    test('should be able to load an existing PPTX (containing six slides) and move slide #2 to slide #6', async () => {
+        try {
+            let testFileName = 'presentation-swap-existing-slides-large-2-to-6.pptx';
+
+            expect.assertions(1);
+
+            if (fs.existsSync(`${tmpDir}/${testFileName}`)) {
+                fs.unlinkSync(`${tmpDir}/${testFileName}`);
+            }
+
+            let pptx = new PPTX.Composer();
+
+            await pptx.load(`${__dirname}/fixtures/six-slides.pptx`);
+            await pptx.compose(async pres => {
+                let slide = pres.getSlide(2);
+                slide.moveTo(6);
             });
 
             await pptx.save(`${tmpDir}/${testFileName}`);
