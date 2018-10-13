b5eca1fb72d82a741b6ac62d4cfe1b31e53d74fa
gregdolley <gregdolley@gmail.com>
2018-10-12 18:31:35 -0700

    Added two tests for reordering slides on PPTX's created from scratch.

--- __tests__/reorder-slide.test.js
+++ __tests__/reorder-slide.test.js
@@ -88,4 +88,70 @@ describe('Presentation Module', () => {
             throw err;
         }
     });
+
+    test('should be able to create a PPTX, add six slides, and move slide #5 to slide #2', async () => {
+        try {
+            let testFileName = 'presentation-create-new-swap-slides-5-to-2.pptx';
+            let pptx = new PPTX.Composer();
+
+            expect.assertions(1);
+
+            await pptx.compose(async pres => {
+                let slide1 = await pres.addSlide();
+                let slide2 = await pres.addSlide();
+                let slide3 = await pres.addSlide();
+                let slide4 = await pres.addSlide();
+                let slide5 = await pres.addSlide();
+                let slide6 = await pres.addSlide();
+
+                slide1.addText({ value: 'Slide 1', x: 200, y: 100 });
+                slide2.addText({ value: 'Slide 2', x: 200, y: 100 });
+                slide3.addText({ value: 'Slide 3', x: 200, y: 100 });
+                slide4.addText({ value: 'Slide 4', x: 200, y: 100 });
+                slide5.addText({ value: 'Slide 5', x: 200, y: 100 });
+                slide6.addText({ value: 'Slide 6', x: 200, y: 100 });
+
+                slide5.moveTo(2);
+            });
+
+            await pptx.save(`${tmpDir}/${testFileName}`);
+            expect(fs.existsSync(`${tmpDir}/${testFileName}`)).toBe(true);
+        } catch (err) {
+            console.warn(err);
+            throw err;
+        }
+    });
+
+    test('should be able to create a PPTX, add six slides, and move slide #2 to slide #6', async () => {
+        try {
+            let testFileName = 'presentation-create-new-swap-slides-2-to-6.pptx';
+            let pptx = new PPTX.Composer();
+
+            expect.assertions(1);
+
+            await pptx.compose(async pres => {
+                let slide1 = await pres.addSlide();
+                let slide2 = await pres.addSlide('slideLayout2');
+                let slide3 = await pres.addSlide();
+                let slide4 = await pres.addSlide();
+                let slide5 = await pres.addSlide();
+                let slide6 = await pres.addSlide('slideLayout6');
+
+                slide1.addText({ value: 'Slide 1', x: 200, y: 100 });
+                slide2.addText({ value: 'Slide 2', x: 200, y: 100 });
+                slide3.addText({ value: 'Slide 3', x: 200, y: 100 });
+                slide4.addText({ value: 'Slide 4', x: 200, y: 100 });
+                slide5.addText({ value: 'Slide 5', x: 200, y: 100 });
+                slide6.addText({ value: 'Slide 6', x: 200, y: 100 });
+
+                slide2.moveTo(6);
+            });
+
+            await pptx.save(`${tmpDir}/${testFileName}`);
+            expect(fs.existsSync(`${tmpDir}/${testFileName}`)).toBe(true);
+        } catch (err) {
+            console.warn(err);
+            throw err;
+        }
+    });
 });
